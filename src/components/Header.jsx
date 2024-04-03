fb74945bdb6678a02d4d94bcc029263cf819ff47
Purushottam <mathadpurushottam@gmail.com>
2024-04-03 20:28:01 +0530

    build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { useAuth } from '../utils/AuthContext'
 import { Link } from 'react-router-dom'
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, {useEffect, useState} from 'react'
 import { useAuth } from '../utils/AuthContext'
 import { useNavigate } from 'react-router'
